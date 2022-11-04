a13f3e534bb82f2bc38a0608d5ee67b7740c1b40
neptune <103205025+neptune0xf@users.noreply.github.com>
2022-11-04 23:11:05 +0800

    fix: left side panel

--- src/components/leftSide/LeftSidePanel.tsx
+++ src/components/leftSide/LeftSidePanel.tsx
@@ -1,38 +1,31 @@
 import nnsdaoLogo from '@/public/home/nnsdaoLogo.ico';
-import { Avatar } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
+import { Avatar, Box, Stack } from '@mui/material';
 import JoinedDao from './joinedDao/Index';
 export default function LeftSidePanel() {
   // const JoinedDaoWrap = LoadingWrapper(joinedDao, useTotalDaoLists);
   return (
-    <>
-      <Grid container direction="column" alignItems="center" spacing={0}>
-        <Grid xl={1} sx={{ marginY: '24px' }}>
-          <Avatar
-            alt="Remy Sharp"
-            src={nnsdaoLogo}
-            sx={{
-              width: 63,
-              height: 63,
-              cursor: 'pointer',
-            }}
-          />
-        </Grid>
-        <Grid xl={10} overflow="auto" disableEqualOverflow height={'500px'}>
-          <JoinedDao />
-        </Grid>
-        <Grid xl={1} sx={{ marginY: '50px' }}>
-          <Avatar
-            alt="Remy Sharp"
-            src={nnsdaoLogo}
-            sx={{
-              width: 63,
-              height: 63,
-              cursor: 'pointer',
-            }}
-          />
-        </Grid>
-      </Grid>
-    </>
+    <Stack spacing={0} alignItems="center" height={'100vh'}>
+      <Avatar
+        alt="Remy Sharp"
+        src={nnsdaoLogo}
+        sx={{
+          width: 63,
+          height: 63,
+          cursor: 'pointer',
+        }}
+      />
+      <Box sx={{ maxHeight: 'calc( 100% - 126px)', background: 'red' }}>
+        <JoinedDao />
+      </Box>
+      <Avatar
+        alt="Remy Sharp"
+        src={nnsdaoLogo}
+        sx={{
+          width: 63,
+          height: 63,
+          cursor: 'pointer',
+        }}
+      />
+    </Stack>
   );
 }
--- src/components/leftSide/joinedDao/Index.tsx
+++ src/components/leftSide/joinedDao/Index.tsx
@@ -1,91 +1,38 @@
-import { Avatar, Box, Grid, Tooltip, Zoom } from '@mui/material';
+import { Avatar, Box, Stack, Tooltip, Zoom } from '@mui/material';
 
 export default function JoinedDao(props) {
   // const data = props.data;
   // const navigator = useNavigate();
-  const data = [
-    {
-      canister: '1',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '2',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '3',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '4',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '5',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '6',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '7',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '8',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '9',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '10',
-      avatar: '',
-      name: '1',
-    },
-    {
-      canister: '12',
-      avatar: '',
-      name: '1',
-    },
-  ];
+  const data = Array.from({ length: 20 }).map((item, i) => ({
+    canister: i + 1,
+    avatar: i + 1,
+    name: i + 1,
+  }));
+
   const toDetailPage = item => {
     // navigator(`/daoDetail/:${item.canister}`);
   };
 
   return (
-    <>
-      <Grid container direction="column" alignItems="center" spacing={0}>
-        {data.map((item, index) => (
-          <Box key={item.canister}>
-            <Tooltip title={item.name} TransitionComponent={Zoom} placement="right" TransitionProps={{ timeout: 200 }}>
-              <Avatar
-                alt="Remy Sharp"
-                src={item.avatar}
-                onClick={() => toDetailPage(item)}
-                sx={{
-                  width: 47,
-                  height: 47,
-                  cursor: 'pointer',
-                  margin: '10px',
-                  '&:hover': { border: '2px solid #39a8d3' },
-                }}
-              />
-            </Tooltip>
-          </Box>
-        ))}
-      </Grid>
-    </>
+    <Stack spacing={1} sx={{ height: '100%', overflowY: 'scroll' }}>
+      {data.map((item, index) => (
+        <Box key={item.canister}>
+          <Tooltip title={item.name} TransitionComponent={Zoom} placement="right" TransitionProps={{ timeout: 200 }}>
+            <Avatar
+              alt="Remy Sharp"
+              src={item.avatar}
+              onClick={() => toDetailPage(item)}
+              sx={{
+                width: 47,
+                height: 47,
+                cursor: 'pointer',
+                margin: '10px',
+                '&:hover': { border: '2px solid #39a8d3' },
+              }}
+            />
+          </Tooltip>
+        </Box>
+      ))}
+    </Stack>
   );
 }
