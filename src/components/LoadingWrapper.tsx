3d214d7c1215ea54c9d83b61a1e0456a59881f55
neptune <103205025+neptune0xf@users.noreply.github.com>
2022-11-21 16:08:46 +0800

    fix: loadingWrapper

--- src/components/LoadingWrapper.tsx
+++ src/components/LoadingWrapper.tsx
@@ -1,23 +1,44 @@
 import RefreshIcon from '@mui/icons-material/Refresh';
 import LoadingButton from '@mui/lab/LoadingButton';
-import { Box, CircularProgress, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import type { UseQueryResult } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { PulseLoader } from 'react-spinners';
 
+let toastID;
 export default (WrappedComponent, queryFn: (...arg: any[]) => UseQueryResult) => {
   const hocComponent = ({ ...props }) => {
     const data = queryFn();
+    useEffect(() => {
+      if (data.isFetching && data.data) {
+        if (toastID) {
+          toastID = toast.loading('refetching...', { id: toastID });
+        } else {
+          toastID = toast.loading('refetching...');
+        }
+      }
+      // else if (!data.isFetching) {
+      //   toastID && toast.dismiss(toastID);
+      // }
+
+      return () => {
+        toastID && toast.dismiss(toastID);
+        toastID = null;
+      };
+    }, [data.isFetching]);
+
     if (data.data) {
       return (
-        <Container maxWidth="xl" sx={{ position: 'relative' }}>
-          <WrappedComponent data={data.data} {...props} />
-          {data.isFetching ? (
-            <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, textAlign: 'center' }}>
-              <CircularProgress />
-            </Box>
-          ) : null}
-        </Container>
+        <WrappedComponent data={data.data} {...props} />
+        // <Container maxWidth="xl" sx={{ position: 'relative' }}>
+        //   {data.isFetching ? (
+        //     <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, textAlign: 'center' }}>
+        //       <CircularProgress />
+        //     </Box>
+        //   ) : null}
+        // </Container>
       );
     }
     if (data.isLoading && !data.isFetched) {
